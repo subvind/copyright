82b221e1d410f7df0a92b4ce990f4032a055580b
c_u_l8er_true ucimecu inomauto <subvind@uni.lan>
2023-07-03 15:44:05 -0500

    boilerplate

---
+++ src/assets/js/script.js
@@ -0,0 +1,14 @@
+
+function main () {
+  console.log('hello world!')
+}
+
+function ready(fn) {
+  if (document.readyState !== 'loading') {
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+
+ready(main)
